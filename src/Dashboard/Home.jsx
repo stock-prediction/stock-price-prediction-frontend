1ca543dd79ae345fc0c4b69ec837796f3a2d21b2
Alok Singh <thealoksinghh@gmail.com>
2025-03-18 18:30:03 +0530

    custom css file added

--- src/Dashboard/Home.jsx
+++ src/Dashboard/Home.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import Navigation from './navigation';
+import "../styles/home.css";
 const Home = () => {
   return (
     <>
     <div className='w-full'><Navigation/></div>
     <div className='w-full h-screen flex'>
-    <div className='w-1/2 bg-teal-200 h-full'></div>
+    <div className='w-1/2 customcss h-full'></div>
     <div className='w-1/2 bg-cyan-200 h-full'></div>
     </div>
     
