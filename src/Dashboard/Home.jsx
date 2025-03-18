e077c47a6cefc0914c4eaac94e42ebf103cd829c
Alok Singh <thealoksinghh@gmail.com>
2025-03-18 18:03:37 +0530

    color changed in home page rose to teal

--- src/Dashboard/Home.jsx
+++ src/Dashboard/Home.jsx
@@ -5,7 +5,7 @@ const Home = () => {
     <>
     <div className='w-full'><Navigation/></div>
     <div className='w-full h-screen flex'>
-    <div className='w-1/2 bg-rose-200 h-full'></div>
+    <div className='w-1/2 bg-teal-200 h-full'></div>
     <div className='w-1/2 bg-cyan-200 h-full'></div>
     </div>
     
